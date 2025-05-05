b3e30dc9acaac869eb32a1912032b33958ad01e9
Dmytro-Soia <dmytro.soia@jobtrek.ch>
2025-05-05 11:08:47 +0200

    feat: added layout to all pages

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en" class="font-playfair overflow-x-hidden">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap"
+          rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
+    <title>{{ $title }}</title>
+</head>
+<body class="h-dvh relative">
+<nav class="bg-pearl-bush-200 flex flex-row items-center px-6 py-4 fixed w-full justify-between z-20">
+    <a href="/" class="text-cognac-800 @if(request()->is("/")) text-5xl @else text-4xl @endif">Not Fake AirBNB</a>
+    <div class="left-1/2 ml-35">
+        @if(request()->is('/'))
+            <div class="flex gap-6 rounded-2xl px-4 py-3 bg-pearl-bush-100 text-black shadow-lg">
+                <div class="flex flex-col items-start">
+                    <label for="where" class="mb-1 font-semibold text-lg">City</label>
+                    <input type="text" id="where" name="where" placeholder="Rome" class="border border-gray-500 transition-colors focus:ring-2 focus:ring-cognac-800 focus:outline-none rounded-md px-2 py-1 w-40"/>
+                </div>
+                <div class="flex flex-col items-start">
+                    <label for="where" class="mb-1 font-semibold text-lg">Min Price</label>
+                    <input type="number" id="where" name="where" placeholder="0" class="border border-gray-500 transition-colors focus:ring-2 focus:ring-cognac-800 focus:outline-none rounded-md px-2 py-1 w-40"/>
+                </div>
+                <div class="flex flex-col items-start">
+                    <label for="where" class="mb-1 font-semibold text-lg">Max Price</label>
+                    <input type="number" id="where" name="where" placeholder="0" class="border border-gray-500 transition-colors focus:ring-2 focus:ring-cognac-800 focus:outline-none rounded-md px-2 py-1 w-40"/>
+                </div>
+                <div class="flex flex-col items-start">
+                    <label for="where" class="mb-1 font-semibold text-lg">Rooms</label>
+                    <input type="number" id="where" name="where" placeholder="0" class="border border-gray-500 transition-colors focus:ring-2 focus:ring-cognac-800 focus:outline-none rounded-md px-2 py-1 w-40"/></div>
+                <div class="flex flex-col items-start">
+                    <label for="persons" class="mb-1 font-semibold text-lg">Persons</label>
+                    <input type="number" id="persons" name="persons" placeholder="0" class="border border-gray-500 transition-colors focus:ring-2 focus:ring-cognac-800 focus:outline-none rounded-md px-2 py-1 w-40"/></div>
+            </div>
+        @endif
+    </div>
+    <div class="flex flex-row space-x-6">
+        <a href="rent" class="py-2 px-4 rounded-2xl bg-cognac-800 text-pearl-bush-200 text-3xl">NFAirBNB your house</a>
+        <div class="flex flex-row py-2 px-4 rounded-2xl bg-cognac-800 text-pearl-bush-200 space-x-3">
+            <a href="register" class="text-3xl p">Register</a>
+            {{--<img src="/images/bb.avif" width="37" class="rounded-4xl">
+            <a href="" class="text-3xl"> Username</a>--}}
+        </div>
+    </div>
+</nav>
+<div class=" h-screen @if(request()->is("/")) pt-35  @else pt-24 @endif px-12">{{ $slot }}</div>
+<script src="https://flowbite.com/docs/flowbite.min.js"></script>
+</body>
+</html>
