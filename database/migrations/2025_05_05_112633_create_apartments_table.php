faad2a7910516cc618426881998fdd13193e45f6
Dmytro-Soia <dmytro.soia@jobtrek.ch>
2025-05-05 14:52:11 +0200

    feat: added migrations and models for apartments and bookings

---
+++ database/migrations/2025_05_05_112633_create_apartments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('apartments', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title');
+            $table->string('photos');
+            $table->string('description');
+            $table->foreignId('owner_id')->constrained('users')->onDelete('cascade');
+            $table->integer('rooms');
+            $table->integer('max_people');
+            $table->integer('price');
+            $table->string('country');
+            $table->string('city');
+            $table->string('street');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('apartments');
+    }
+};
