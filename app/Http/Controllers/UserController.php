61614325f07723b61bd2abb4656a6239ccd3a5a2
Dmytro-Soia <dmytro.soia@jobtrek.ch>
2025-05-06 15:20:17 +0200

    feat: add user registration and authentification

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    public function authenticate(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'name' => ['required', 'max:255'],
+            'email' => ['required', 'email', 'unique:users'],
+            'password' => ['required', 'confirmed'],
+        ]);
+
+        User::create($request->all());
+
+        return redirect('login');
+    }
+
+    public function login(Request $request): RedirectResponse
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->intended('/');
+        }
+    return back()->withErrors([
+        'email' => 'The provided credential do not match our records:(',
+    ])->onlyInput('email');
+    }
+}
