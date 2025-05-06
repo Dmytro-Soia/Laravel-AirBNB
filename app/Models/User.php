895cfe8f856ee1459201132d5a07ca3d20210d3f
Dmytro-Soia <dmytro.soia@jobtrek.ch>
2025-05-06 13:42:14 +0200

    feat: add Booking model, factory, and relationships

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -49,5 +50,10 @@ class User extends Authenticatable
     public function apartments(): \Illuminate\Database\Eloquent\Relations\HasMany
     {
         return $this->hasMany(Apartment::class, 'owner_id');
+    }
+
+    public function bookings(): HasMany
+    {
+        return $this->hasMany(Booking::class, 'tenant_id');
     }
 }
---
+++ database/factories/BookingFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Apartment;
+use App\Models\Booking;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<Booking>
+ */
+class BookingFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'reserved_at' => fake()->dateTimeBetween('today', 'tomorrow'),
+            'expired_at' => fake()->dateTimeBetween('tomorrow', 'tomorrow'),
+        ];
+    }
+}
